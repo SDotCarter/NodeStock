9cf0b94157958ec8fbc7c23289eca16fe3a463e5
Sean Carter <seancarterjones@hotmail.com>
2020-01-22 05:50:37 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+const PORT = process.env.PORT || 5000;
+
+//Set Handlebars Middleware
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+const otherStuff = "Hello there, this is other stuff.";
+
+//Set Handlebar Routes
+app.get("/", function(req, res) {
+  res.render("home", {
+    stuff: otherStuff
+  });
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => console.log("Server listening on port " + PORT));
